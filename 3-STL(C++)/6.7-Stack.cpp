a9d43869f6587f4f0deb021b63c280ec18432b34
TianshuoLi <lts60892578@163.com>
2023-04-11 10:28:49 +0800

    update stack.emplace()

--- 3-STL(C++)/6.7-Stack.cpp
+++ 3-STL(C++)/6.7-Stack.cpp
@@ -1,8 +1,9 @@
-#include<stack>
+#include<bits/stdc++.h>
+using namespace std;
 
-1.定义
+//1.定义
     stack<typename> st;
-2.访问
+//2.访问
     后进后出的数据结构，只能访问栈顶元素
     st.top();
 3.函数
@@ -15,6 +16,13 @@
     (4) st.empty();
 
     (5) st.size();
+
+
+    st.emplace();//构造元素并插入
+    //例如：
+    stack<pair<string,int> >st;
+    st.emplace("abc",3)
+
 
 4.用途
     模拟递归等
