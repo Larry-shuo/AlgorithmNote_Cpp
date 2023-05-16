4d74d8ebaa942807e8bf22a8d73cff2655b32b73
TianshuoLi <lts60892578@163.com>
2023-05-16 18:00:25 +0800

    update accumulate

--- 3-STL(C++)/STL tools.cpp
+++ 3-STL(C++)/STL tools.cpp
@@ -1,8 +1,15 @@
-
-
-1.itoa()实现对容器元素的批量赋值
-头文件<numeric>
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+//实现对容器元素的批量赋值
+itoa();
 //数组
 iota(array, array+length[array], number);
 //vector容器
 iota(vector.begin(), vector.end(), number);//number为初值,第一个元素值
+
+
+//求和
+int sum = accumulate(v.begin(),v.emd(),0);//第三个参数为累计的初值
+
+string sum = accumulate(v.begin(), v.end(), string(""));
